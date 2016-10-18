57025709a60b1fcc78bb6da7ac8ee9ea7ebd0ff4
Kacper Kowalski <kowal@Mac-mini-Kacper.local>
2016-10-18 16:48:38 +0200

    parsing csv file

--- pokedex/ViewController.swift
+++ pokedex/ViewController.swift
@@ -12,21 +12,45 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
 UICollectionViewDelegateFlowLayout{
 
     @IBOutlet weak var collection: UICollectionView!
+    
+    var pokemon = [Pokemon]()
    
     override func viewDidLoad() {
         super.viewDidLoad()
         collection.delegate = self          // conform to the protocol
         collection.dataSource = self
        
+        parsePokemonCSV()                   //  when it;s called viedidLoad it will do the parsing
     }
     
+    func parsePokemonCSV() {                // parsing a CSV file, grab the data from this file and parse it
+        let path = Bundle.main.path(forResource: "pokemon", ofType: "csv")!
+                                                // it can throw error -> so do
+        do {
+            let csv = try CSV(contentsOfURL: path)
+            let rows = csv.rows             // print the rows of csv file
+                                            // iterate through the rows and create pokemon objects and put them in a array
+            for row in rows {
+                let pokeId = Int(row["id"]!)! // row = dictionairy
+                let name = row["identifier"]!
+                let poke = Pokemon(name: name, pokedexId: pokeId)
+                pokemon.append(poke)
+            }
+            
+            
+            
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+        
+    }
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
         if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "PokeCell", for: indexPath) as? PokeCell {
             // indexPath - index of the row -> grab it and cast as PokeCell!
             
-           let pokemon = Pokemon(name: "test", pokedexId: indexPath.row)
-            cell.configureCell(pokemon: pokemon)
+            var poke = pokemon[indexPath.row]
+            cell.configureCell(pokemon: poke)
             
             return cell
         } else {
